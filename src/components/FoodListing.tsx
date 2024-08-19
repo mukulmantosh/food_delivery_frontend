1ec7f82bf7cccee0cd3c9613b0f5dea74d9c72c4
Mukul Mantosh <mukulmantosh@yahoo.com>
2024-08-19 16:37:24 +0530

    Initial Commit

---
+++ src/components/FoodListing.tsx
@@ -0,0 +1,41 @@
+
+function FoodListing(){
+    return (
+        <div>
+            <div className="container is-fluid mt-6 pt-6">
+                <div className="fixed-grid has-5-cols">
+                    <div className="grid">
+                        <div className="cell">
+                            <div className="card">
+                                <div className="card-image">
+                                    <figure className="image is-4by3">
+                                        <img
+                                            src="https://bulma.io/assets/images/placeholders/1280x960.png"
+                                            alt="Placeholder image"
+                                        />
+                                    </figure>
+                                </div>
+                                <div className="card-content">
+                                    <div className="media">
+                                        <div className="media-content">
+                                            <p className="title is-4">Burger</p>
+                                            <p className="subtitle is-6">Burger</p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default FoodListing;
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+
+function Header() {
+    return (
+        <div>
+            <div className="container">
+                <div className="has-text-centered mt-5">
+                    <h1 className="title is-size-1">Go Eats</h1>
+                    <p className="subtitle is-size-3">Smoky Goodness in Every Bite</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
