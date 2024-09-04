ffcaa6dc275e83fda3b2be6bf14583fb65f07340
Mukul Mantosh <mukulmantosh@yahoo.com>
2024-09-04 15:17:53 +0530

    Update UserIcon component to dynamically set image URL
    Removed unused 'user' variable and added dynamic image URL based on authentication state. This improves the clarity and functionality of the UserIcon component.

--- src/components/UserIcon.tsx
+++ src/components/UserIcon.tsx
@@ -2,22 +2,22 @@ import useAuth from "../context/auth.tsx";
 
 
 function UserIcon() {
-    const { isAuthenticated, user } = useAuth();
+    const { isAuthenticated } = useAuth();
 
     let link: string;
+    let imageUrl: string;
     if (isAuthenticated) {
+        imageUrl = "/images/logout.png"
         link = "/user/logout"
     } else {
+        imageUrl = "/images/user-128.svg"
         link = "/user/register"
     }
     return (
-
     <figure className="image is-48x48 mt-5">
-
         <a href={link}>
-            <img alt="user" src="/images/user-128.svg"/>
+            <img alt="user" src={imageUrl}/>
         </a>
-        <span>{user}</span>
     </figure>
     )
 }
